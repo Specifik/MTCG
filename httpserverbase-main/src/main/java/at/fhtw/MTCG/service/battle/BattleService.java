1ce14f82136af445566b57cf9fbbb83ddd5592f1
Specifik <andymih@yahoo.de>
2025-01-31 07:53:41 +0100

    battle demo deleted

--- httpserverbase-main/src/main/java/at/fhtw/MTCG/service/battle/BattleService.java
+++ httpserverbase-main/src/main/java/at/fhtw/MTCG/service/battle/BattleService.java
@@ -7,13 +7,9 @@ import at.fhtw.httpserver.server.Response;
 import at.fhtw.httpserver.server.Service;
 
 public class BattleService implements Service {
-    private final BattleController battleController = new BattleController();
 
     @Override
     public Response handleRequest(Request request) {
-        if ("POST".equalsIgnoreCase(request.getMethod().toString())) {
-            return battleController.startBattle(request);
-        }
-        return new Response(HttpStatus.METHOD_NOT_ALLOWED, ContentType.JSON,"{ \"message\": \"Method Not Allowed\" }");
+        return null;
     }
 }
